f1d035152015b4c45cd7d09b586ba1b80200daea
Eva Borrajo de Orozco Gandia <EBORDEO@upv.edu.es>
2026-04-22 01:02:08 +0200

    Modelo empezado

--- model/train.py
+++ model/train.py
@@ -1,1 +1,15 @@
-print("Nada")
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+#Cargamos datos
+df = pd.read_csv("data/processed/mental_health.csv")
+
+X = df["text"]
+y = df["label"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42, stratify=y)
+
+print("Modelo en curso")
